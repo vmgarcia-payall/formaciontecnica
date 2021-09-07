5a274e322051dafbdb7fa6373d5a79def56f36b1
Victor Garcia <vmgarcia18@hotmail.com>
2021-09-07 20:49:04 +0000

    Space Module

---
+++ odoo_space/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Odoo Space',
+    'summary': """App to manage space missions""",
+    'description': """Test :D""",
+    'author': 'Victor Garcia',
+    'category': 'Space',
+    'version': '0.1',
+    'depends': ['base'],
+}
